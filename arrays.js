bc515f04d8c0e2ca5a5ac0a1fa095553df1b5a1e
Bart Sakowski <bart.sakowski@gmail.com>
2020-02-18 03:47:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 
 function accessElementInArray(array, index) {
   console.log(array[index]);
-  return()
+  return(index)
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
