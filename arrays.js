fa71cc97b81ea8eb2a245f5acb00ae81781f8069
Bart Sakowski <bart.sakowski@gmail.com>
2020-02-18 03:26:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,11 @@ function addElementToBeginningOfArray(array, element) {
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
 }
+
+function addElementToEndOfArray(array, element) {
+  [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+}
